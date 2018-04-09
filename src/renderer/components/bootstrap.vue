9afb660c9d3e6e908c40a8375fd6c841f7847e2b
BlueboosuphuSKI <huangxuewu@gmail.com>
2018-04-09 01:01:46 -0400

    *add caller id name filter

--- src/renderer/components/bootstrap.vue
+++ src/renderer/components/bootstrap.vue
@@ -176,6 +176,18 @@ export default {
             let name = raw.find(i => i.indexOf("NAME") !== -1);
             name = name ? name.split("=")[1].replace(/[\r\n]/g, "") : "";
 
+            if (
+              [
+                "AVAILABLE",
+                "UNAVAILA",
+                "WIRELESS",
+                "CELL PHONE",
+                "UNKNOWN"
+              ].includes(name.toUpperCase())
+            ) {
+              name = "";
+            }
+
             const phone = raw
               .find(i => i.indexOf("NMBR") !== -1)
               .split("=")[1]
